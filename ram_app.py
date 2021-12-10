00233b58c2cea53528de9aa5f68aaccaa32f75c7
sairam1551 <95556221+sairam1551@users.noreply.github.com>
2021-12-10 13:21:10 -0500

    Update ram_app.py

--- ram_app.py
+++ ram_app.py
@@ -38,43 +38,32 @@ class Timespent:
         except ValueError:
             return "N"
     def main(self,Data_i):
-    workTime = re.compile(r'((\d{0,1})\d:\d\d)(am|pm)( )*-( )*((\d{0,1})\d:\d\d)(am|pm)')
-    timeElapsedInMinutes = 0
-    flag = True
-    line_number = 0;
-    for line in Data_i:
-        line_number += 1
-        if (line.find("Time Log") != -1):
-          flag = False
-          continue
-        if flag:
-          continue
-        line = line.lower()
-        line = line.strip()
-        if line and workTime.search(line):
-          time = workTime.search(line)
-          startTime = time.group(1)
-          startTimeMeridiem = time.group(3)
-          endTime = time.group(6)
-          endTimeMeridiem = time.group(8)
-
-          #for start time
-          timeString = startTime.split(":")
-          if(timeString[0] == "12"):
-            timeString[0] = "0"
-          startTimeInMinutes = int(timeString[0])*60 + int(timeString[1]) + (0 if startTimeMeridiem=="am" else 720)
-
-          #for end time
-          timeString = endTime.split(":")
-          if(timeString[0] == "12"):
-            timeString[0] = "0"
-          endTimeInMinutes = int(timeString[0])*60 + int(timeString[1]) + (0 if endTimeMeridiem=="am" else 720)
-          
-          timeElapsedInMinutes += endTimeInMinutes - startTimeInMinutes if endTimeInMinutes > startTimeInMinutes else endTimeInMinutes - startTimeInMinutes + 1440
-
+        cl_initial = ''
+        ftd = []
+        for currentline, ldata in enumerate(Data_i):
+            if ldata.strip('\n').count("Time Log"):
+                cl_initial = currentline
+                break
+            else:
+                cl_initial = 'TimeLog'
+        if cl_initial != "TimeLog":
+            for iv in range(int(cl_initial), len(Data_i)):
+                fsd = (Data_i[iv].split(' - ')[0].split())
+                ssd = (Data_i[iv].split(' - ')[1:])
+                counter = (Data_i[iv].split(' - ')[1:])
+                if len(fsd) != 0:
+                    timeformatstatus = self.timechange(fsd[-1].strip())
+                    if len(counter) != 0:
+                        timeformatstatus_v1 = self.timechange(ssd[0].split()[0])
+                        if timeformatstatus_v1 == "Y" and timeformatstatus == "Y":
+                            ftd.append((fsd[-1], str(ssd[0].split()[0])))
+                    else:
+                        st.write("Time stamp not present in the line: ", iv + 1)
+                else:
+                    st.write("No time stamp present in the line: ", iv + 1)
+            self.TimeTotal(ftd)
         else:
-          st.write('Could not parse time in line '+str(line_number))
-    st.write("Total time author spent : " + str(timeElapsedInMinutes//60) +" hrs " + str(timeElapsedInMinutes%60) + " minutes")
+            st.write("Time Log not in the file : ", fname)
 if __name__ == '__main__':
     st.title("Webapp for tl Parser")
     main_bg = "4397636.jpg"
